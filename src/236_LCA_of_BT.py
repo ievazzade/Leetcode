9eeaffb3b97eee2339944e660609768c33c0c629
ievazzade <i.evazzade@gmail.com>
2022-07-09 23:29:55 -0500

    lca update

--- src/236_LCA_of_BT.py
+++ src/236_LCA_of_BT.py
@@ -16,3 +16,19 @@ class Solution:
         
         recurse_tree(root)
         return self.ans
+
+# https://www.youtube.com/watch?v=py3R23aAPCA&t=924s
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root.val == p.val or root.val == q.val:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p,q)
+        right = self.lowestCommonAncestor(root.right, p,q)
+        
+        if left == None: return right
+        if right == None: return left
+        
+        return root
