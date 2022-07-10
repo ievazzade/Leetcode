b71b7f666776e4decd5086d6bd5eb26f8c45df88
ievazzade <i.evazzade@gmail.com>
2022-07-10 17:49:56 -0500

    637 Update

--- src/637_Average_Level_BT.py
+++ src/637_Average_Level_BT.py
@@ -28,3 +28,32 @@ class Solution:
         for i in range(len(ans)):
             res.append(ans[i][0]/ans[i][1])
         return res
+
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        """
+        [3,9,20,null,null,15,7]
+        
+        [[3, 1], [29, 2], [22, 2]]
+        """
+        
+        res = []
+        if not root:
+            return res
+        queue = deque([root])
+        
+        while queue:
+            sum_level = 0
+            length = len(queue)
+            for i in range(length):
+                node = queue.popleft()
+                sum_level += node.val
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            res.append(sum_level/length)
+        
+        return res
