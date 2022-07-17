d1c1ae1feece5c31d8e0c6e331158aee0b3eddd2
ievazzade <i.evazzade@gmail.com>
2022-07-17 15:15:06 -0500

    215 heapq

--- src/215_Kth_Largest_Array.py
+++ src/215_Kth_Largest_Array.py
@@ -1,4 +1,13 @@
+# Sorting the array
+# O(nlogn)
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         nums.sort()
         return nums[len(nums) - k]
+
+# Heap of size K
+#O(nlogk)
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return heapq.nlargest(k, nums)[-1]
