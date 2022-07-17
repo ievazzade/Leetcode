660914bba880bd0635f11384de75465830d3c7c8
ievazzade <i.evazzade@gmail.com>
2022-07-16 20:29:29 -0500

    50 fast pow

--- src/50_Pow_X.py
+++ src/50_Pow_X.py
@@ -7,3 +7,22 @@ class Solution:
         for i in range(n):
             ans *= x
         return ans
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        def fastPow(x, n):
+            if n == 0:
+                return 1
+            half = fastPow(x, n//2)
+            
+            if n % 2 == 0:
+                return half * half
+            else:
+                return half * half * x
+        
+        if n < 0:
+            x = 1.0 / x
+            n *= -1
+        
+        return fastPow(x, n)
