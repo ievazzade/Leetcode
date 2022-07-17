1827b6839138d95e856efb778cb9fcc7619f7268
ievazzade <i.evazzade@gmail.com>
2022-07-17 15:52:42 -0500

    215 Quick Select

--- src/215_Kth_Largest_Array.py
+++ src/215_Kth_Largest_Array.py
@@ -11,3 +11,27 @@ class Solution:
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return heapq.nlargest(k, nums)[-1]
+
+# QuickSelect
+# Average O(n); Worst O(n^2)
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        """
+        [3,2,1,5,6,4]
+         
+        """
+        k = len(nums) - k
+        
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] < pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+            
+            if p > k: return quickSelect(l, p - 1)
+            elif p < k: return quickSelect(p + 1, r)
+            else: return nums[p]
+        
+        return quickSelect(0, len(nums) - 1)
