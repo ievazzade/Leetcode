03ac644da3f4c433c5c38f2f74f8fd1ea9442851
ievazzade <i.evazzade@gmail.com>
2022-07-17 20:08:50 -0500

    973 update: sort key

--- src/973_K_Closet_Points_to_Origin.py
+++ src/973_K_Closet_Points_to_Origin.py
@@ -18,3 +18,12 @@ class Solution:
                 break
         
         return ans
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key = self.squared_distance)
+        
+        return points[:k]
+    
+    def squared_distance(self, point):
+        return point[0] ** 2 + point[1] ** 2
