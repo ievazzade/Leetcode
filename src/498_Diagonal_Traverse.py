de303237f8f7549170fcab480d38941890ce39c0
ievazzade <i.evazzade@gmail.com>
2022-07-24 09:44:21 -0500

    498 Update space complexity O(1)

--- src/498_Diagonal_Traverse.py
+++ src/498_Diagonal_Traverse.py
@@ -27,3 +27,37 @@ class Solution:
                 result.extend(diagonal)
 
         return result
+
+class Solution:
+    def findDiagonalOrder(self, mat):
+        if not mat or not mat[0]:
+            return []
+        
+        m, n = len(mat), len(mat[0])
+
+        result = []
+
+        row, col = 0, 0
+        direction = 1
+        result = []
+        
+        while row < m and col < n:
+            result.append(mat[row][col])
+            
+            new_row = row + (-1 if direction == 1 else 1)
+            new_col = col + (1 if direction == 1 else -1)
+            
+            if new_row < 0 or new_row == m or new_col < 0 or new_col == n:
+                if direction:
+                    row += (col == n - 1)
+                    col += (col < n - 1)
+                else:
+                    col += (row == m - 1)
+                    row += (row < m - 1)
+                    
+                direction = 1 - direction
+            else:
+                row = new_row
+                col = new_col
+        
+        return result
