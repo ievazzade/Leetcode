447aaf8925ffd933d66ff93062e2ce34fb37035d
ievazzade <i.evazzade@gmail.com>
2022-07-27 17:57:35 -0500

    88 update

--- src/88_Merge_Sotred_Array.py
+++ src/88_Merge_Sotred_Array.py
@@ -19,3 +19,25 @@ class Solution:
             else:
                 nums1[i] = nums2[p2]
                 p2 -= 1
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+             v
+        [1,2,2,3,5,6]
+           i
+        [2,5,6]
+         j
+        """
+        p1, p2 = m - 1, n - 1
+        for i in range(m + n - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[i] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[i] = nums2[p2]
+                p2 -= 1
