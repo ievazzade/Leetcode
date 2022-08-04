ac1b87ed8eb9f07a9ae51bc653b7ce17279f9f66
ievazzade <i.evazzade@gmail.com>
2022-08-04 15:01:08 -0500

    958 Update dfs

--- src/958_Check_Complete_BT.py
+++ src/958_Check_Complete_BT.py
@@ -46,3 +46,28 @@ class Solution:
             bfs.append(bfs[i].right)
             i += 1
         return not any(bfs[i:])
+
+
+#DFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        self.count = 0
+        self.max_pos = 0
+        
+        self.dfs(root, 1)
+        return self.count == self.max_pos
+    
+    def dfs(self, root, position):
+        if not root:
+            return 
+        self.count += 1
+        self.max_pos = max(self.max_pos, position)
+        self.dfs(root.left, 2 * position)
+        self.dfs(root.right, 2 * position + 1)
