401ecf7dfc4b59c48b00fd76db089056aac7db37
ievazzade <i.evazzade@gmail.com>
2022-08-07 16:24:32 -0500

    426 Iterative  DFS

--- src/426_Convert_BST_DLL.py
+++ src/426_Convert_BST_DLL.py
@@ -40,3 +40,37 @@ class Solution:
         last.right = first
         
         return first
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+class Solution:
+    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        """
+        prev = 2
+        node = 3
+        stack = [4]
+        """
+        
+        if not root:
+            return None
+        
+        dummy = (0, None, None)
+        prev = dummy
+        stack, node = [], root
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            node.left, prev.right, prev = prev, node, node
+            node = node.right
+        dummy.right.left, prev.right = prev, dummy.right
+        return dummy.right
