3e7b5d464d47ccff950f574d008598bb76b2ef27
ievazzade <i.evazzade@gmail.com>
2022-08-15 11:31:56 -0500

    129 Sum Root Leaf REC

--- src/129_Sum_Root_Leaf.py
+++ src/129_Sum_Root_Leaf.py
@@ -20,3 +20,27 @@ class Solution:
                     stack.append((node.right, cur_number))
         
         return root_to_leaf
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        root_to_leaf = 0
+        
+        def dfs_preorder(node, cur):
+            nonlocal root_to_leaf
+            if node:
+                cur = cur * 10 + node.val
+                if not (node.left or node.right):
+                    root_to_leaf += cur
+                dfs_preorder(node.left, cur)
+                dfs_preorder(node.right, cur)
+        dfs_preorder(root, 0)
+        
+        return root_to_leaf
+
