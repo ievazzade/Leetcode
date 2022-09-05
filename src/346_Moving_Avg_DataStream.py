bee65e45d53bdb0b544ed5195bbcba2fdf297f5e
ievazzade <i.evazzade@gmail.com>
2022-09-05 16:51:32 -0500

    346 my solution update

--- src/346_Moving_Avg_DataStream.py
+++ src/346_Moving_Avg_DataStream.py
@@ -32,3 +32,21 @@ class MovingAverage:
         self.window_sum = self.window_sum - tail + val
         
         return self.window_sum / min(self.size, self.count)
+
+# My Solution
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.size = size
+        self.queue = deque([])
+        
+    def next(self, val: int) -> float:
+        self.queue.append(val)
+        if len(self.queue) > self.size:
+            self.queue.popleft()
+        return sum(self.queue)/len(self.queue)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
