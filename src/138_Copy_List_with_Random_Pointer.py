b85a93ae8fd8b5b96d5dedda6fe4866a8a9d2dfc
ievazzade <i.evazzade@gmail.com>
2022-09-06 17:46:45 -0500

    138 Update Space O(1)

--- src/138_Copy_List_with_Random_Pointer.py
+++ src/138_Copy_List_with_Random_Pointer.py
@@ -93,3 +93,43 @@ class Solution:
             new_node = new_node.next
         
         return self.visited[head]
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return head
+        
+        ptr = head
+        while ptr:
+            new_node = Node(ptr.val, None, None)
+            new_node.next = ptr.next
+            ptr.next = new_node
+            ptr = new_node.next
+        
+        ptr = head
+        
+        while ptr:
+            ptr.next.random = ptr.random.next if ptr.random else None
+            ptr = ptr.next.next
+            
+        ptr_old_list = head
+        ptr_new_list = head.next
+        head_new = head.next
+        
+        while ptr_old_list:
+            ptr_old_list.next = ptr_old_list.next.next
+            ptr_new_list.next = ptr_new_list.next.next if ptr_new_list.next else None
+            ptr_old_list = ptr_old_list.next
+            ptr_new_list = ptr_new_list.next
+        
+        return head_new
