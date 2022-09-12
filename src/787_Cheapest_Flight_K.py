ae1e1f471ee0a066d95fe40d7ba0cef868951923
ievazzade <i.evazzade@gmail.com>
2022-09-12 01:50:54 -0500

    787 Update Knowledge Center

--- src/787_Cheapest_Flight_K.py
+++ src/787_Cheapest_Flight_K.py
@@ -16,3 +16,23 @@ class Solution:
             prices = tmpPrices
         
         return prices[dst] if prices[dst] != float('inf') else -1
+
+
+class Solution:
+    def findCheapestPrice(self, n, flights, src , dst, K):
+        adj = {u: [] for _ in range(n)}
+        for f in flights:
+            adj[f[0]].append((f[1], f[2]))
+
+        q = []
+        q.append((0, src, K + 1))
+        
+        while len(q) > 0:
+            top = heapq.heqpop(q)
+            price, cur, stop  = top
+            if dst == cur: return price
+            if stop > 0:
+                for v in adj[cur]:
+                    heapq.heappush(q, (price + v[1], v[0], stop - 1))
+
+        return -1
