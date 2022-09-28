cb8544607ebe1b36b5d4e9367b6ffc669745d966
ievazzade <i.evazzade@gmail.com>
2022-09-28 16:25:03 -0500

    347 Top K Freq (Bucket Sort)

--- src/347_Top_K_Freq_Elements.py
+++ src/347_Top_K_Freq_Elements.py
@@ -71,3 +71,24 @@ class Solution:
         quickSelect(0, n - 1, n - k)
         
         return unique[n - k:]
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        
+        
+        for n, c in count.items():
+            freq[c].append(n)
+            
+        res = []
+        
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
