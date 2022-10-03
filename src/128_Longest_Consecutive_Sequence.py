3a7747117782a758b12b40ca13ee00b27987d25d
ievazzade <i.evazzade@gmail.com>
2022-10-03 14:33:58 -0500

    128 Brute Force

--- src/128_Longest_Consecutive_Sequence.py
+++ src/128_Longest_Consecutive_Sequence.py
@@ -1,5 +1,22 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+        
+        for num in nums:
+            current_num = num
+            current_streak = 1
+            
+            while current_num + 1 in nums:
+                current_num += 1
+                current_streak += 1
+            
+            longest_streak = max(longest_streak, current_streak)
+        
+        return longest_streak
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
         if not nums:
             return 0
         
@@ -17,3 +34,4 @@ class Solution:
                     current_streak = 1
         
         return max(longest_streak, current_streak)
+
