f9c35dc2a1d7b65dd53a902a84fe34e9808c5fac
ievazzade <i.evazzade@gmail.com>
2022-10-03 23:23:33 -0500

    202 Floyd cycle Find

--- src/202_Happy_Number.py
+++ src/202_Happy_Number.py
@@ -15,3 +15,22 @@ class Solution:
             n = get_next(n)
         
         return n == 1
+
+# Floyd's Cycle-Finding Algorithm
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        def get_next(n):
+            total_sum = 0
+            while n > 0:
+                n, digit = divmod(n, 10)
+                total_sum += digit * digit
+            return total_sum
+        
+        slow = n
+        fast = get_next(n)
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)
+            fast = get_next(get_next(fast))
+        
+        return fast == 1
